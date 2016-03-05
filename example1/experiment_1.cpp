9cc3277e4ff7fae04c46a6119b2c3321e7d9bb45
verdastelo <verda_stelo@hotmail.com>
2016-03-06 01:09:52 +0530

    Example 1 finished.

---
+++ example1/experiment_1.cpp
@@ -0,0 +1,11 @@
+// experiment_1.cpp: Chetan Anand
+// Description: This program prints "Hello World!"
+
+#include <iostream> 
+using namespace std; 
+
+int main() {
+  cout << "Hello World!" << endl; 
+  return 0;
+}
+
---
+++ example1/experiment_3.cpp
@@ -0,0 +1,21 @@
+// experiment_3.cpp: Chetan Anand
+// Description: A program that prints "Hello World!" (left-aligned) 4 times on a line for 6 lines, where each is printed in a field of 17 spaces.
+// Google's solution: https://developers.google.com/edu/c++/solutions/1-2
+// Difference: Google uses cout << setiosflags (ios::left) before the for-loops. 
+
+#include <iostream> 
+using namespace std; 
+
+#include <iostream>
+#include <iomanip> 
+using namespace std; 
+
+int main() {
+  for (int i = 0; i <= 6; i++) {
+      for (int j = 0; j <= 4; j++) {
+            cout << setw (17) << left << "Hello World! ";
+      }
+      cout << endl; 
+  }
+  return 0;
+}
